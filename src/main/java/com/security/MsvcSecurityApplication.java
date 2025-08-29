6d105488c3ac004745fce5fadfcc76d796be045a
Raydberg Gabriel <raydbergg@gmail.com>
2025-08-28 19:50:34 -0500

    configuration

--- src/main/java/com/security/MsvcSecurityApplication.java
+++ src/main/java/com/security/MsvcSecurityApplication.java
@@ -1,9 +1,9 @@
 package com.security;
-
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class MsvcSecurityApplication {
 
     public static void main(String[] args) {
